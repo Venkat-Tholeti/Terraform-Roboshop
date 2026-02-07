e51eefe76d35b708b753764548f2a45e477c1330
venkat <devopsaws84s@gmail.com>
2026-02-07 19:03:08 +0530

    Terraform

--- 02-SG/parameters.tf
+++ 02-SG/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "Frontend_sg_id" {
   name  = "/${var.Project}/${var.Environment}/Frontend_sg_id"
   type  = "String"
-  value = module.frontend.sg_id
+  value = module.Frontend.sg_id
 }
