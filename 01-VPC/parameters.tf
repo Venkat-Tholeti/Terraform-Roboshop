cb3bce0ab5647646d6e2d0b9af19dfbbedd98ba0
venkat <devopsaws84s@gmail.com>
2026-02-07 21:53:22 +0530

    Terraform

--- 01-VPC/parameters.tf
+++ 01-VPC/parameters.tf
@@ -3,3 +3,22 @@ resource "aws_ssm_parameter" "vpc_id" {
   type  = "String"
   value = module.vpc.vpc_id
 }
+
+
+resource "aws_ssm_parameter" "public_subnet_id" {
+  name  = "/${var.Project}/${var.Environment}/public_subnet_id"
+  type  = "StringList"
+  value = join(",", module.vpc.public_subnet_id)
+}
+
+resource "aws_ssm_parameter" "private_subnet_id" {
+  name  = "/${var.Project}/${var.Environment}/private_subnet_id"
+  type  = "StringList"
+  value = join(",", module.vpc.private_subnet_id)
+}
+
+resource "aws_ssm_parameter" "database_subnet_id" {
+  name  = "/${var.Project}/${var.Environment}/database_subnet_id"
+  type  = "StringList"
+  value = join(",", module.vpc.database_subnet_id)
+}
