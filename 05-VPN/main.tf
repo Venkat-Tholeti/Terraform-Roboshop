664e30ffcecb93b7b8df24a83fed0c41501b8a2a
venkat <devopsaws84s@gmail.com>
2026-02-08 15:52:02 +0530

    Terraform

--- 05-VPN/main.tf
+++ 05-VPN/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "VPN" {
   instance_type = "t3.micro"
   vpc_security_group_ids  = [local.VPN_sg_id]
   subnet_id = local.public_subnet_id
-  key_name = "aws_key_pair.VPN.key_name" #KEY PAIR NAME, MAKE SURE THIS EXISTS IN AWS
+  key_name = aws_key_pair.VPN.key_name #KEY PAIR NAME, MAKE SURE THIS EXISTS IN AWS
   #key_name = vpn #If key is already present in aws use this or else use above to import the key to aws
   user_data = file("openvpn.sh")
 
