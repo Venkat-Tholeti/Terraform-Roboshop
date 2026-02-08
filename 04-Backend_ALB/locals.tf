fc370891b05226ee226cf52e01929be1b5060612
venkat <devopsaws84s@gmail.com>
2026-02-08 10:23:18 +0530

    Terraform

--- 04-Backend_ALB/locals.tf
+++ 04-Backend_ALB/locals.tf
@@ -4,9 +4,9 @@ locals {
     Environment = var.Environment
     TERRAFORM = true
   }
-  
+
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  private_subnet_id = split("," , data.aws_ssm_parameter.public_subnet_id.value)
+  private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_id.value)
   backend_alb_sg_id = data.aws_ssm_parameter.Backend_ALB_sg_id.value
 }
 
