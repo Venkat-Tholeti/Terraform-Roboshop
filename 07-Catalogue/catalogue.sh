826f014f422022716f3d6dea529c71e5ebc67c72
venkat <devopsaws84s@gmail.com>
2026-02-13 12:15:47 +0530

    Terraform

--- 07-Catalogue/catalogue.sh
+++ 07-Catalogue/catalogue.sh
@@ -2,4 +2,4 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/daws-84s/ansible-roboshop-roles-tf.git -e component=$1 Main.yml
+ansible-pull -U https://github.com/Venkat-Tholeti/Ansible-Roles-Roboshop.git -e component=$1 Main.yml
