0da03f0e6f69b01cc554a5b98aef757fcb89dbcb
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-04 21:54:54 +0530

    Terraform Roboshop

--- 03-BastionHost/Main.tf
+++ 03-BastionHost/Main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion" {
    ami = local.ami_id
    instance_type = var.instance_size
-   vpc_security_group_ids = local.bastion_sg_id
+   vpc_security_group_ids = [local.bastion_sg_id]
    subnet_id = local.public_subnet_ids
    
    tags = merge(
--- 03-BastionHost/Provider.tf
+++ 03-BastionHost/Provider.tf
@@ -8,7 +8,7 @@ terraform {
 
     backend "s3" {
         bucket         = "my-terraform-state-bucket-vt" 
-        key            = "SG-Roboshop"
+        key            = "Bastion-Roboshop"
         region         = "us-east-1"
         encrypt        = true
         use_lockfile   =  true
