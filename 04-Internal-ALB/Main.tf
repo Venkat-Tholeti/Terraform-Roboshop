e9b6257a066aa5091aefe6340240168e09f34cfe
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-07 13:33:35 +0530

    Terraform-Roboshop

--- 04-Internal-ALB/Main.tf
+++ 04-Internal-ALB/Main.tf
@@ -9,6 +9,7 @@ module "Internal_ALB" {
   subnets = local.private_subnet_ids
   create_security_group = false
   security_groups = [local.Internal_ALB_sg_id]
+  enable_deletion_protection = false
  
   
   tags = merge(
