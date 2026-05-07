d005fa63a09f7329215c92cdd3f7d54858a5e334
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-07 17:49:24 +0530

    Terraform-Roboshop

--- 07-Catalogue/Locals.tf
+++ 07-Catalogue/Locals.tf
@@ -1,7 +1,7 @@
 locals {
   ami_id = data.aws_ami.joindevops.id
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
 
 }
