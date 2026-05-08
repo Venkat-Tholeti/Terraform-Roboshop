d1751fe7022fdef8696436b9060946e405a2880f
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-08 21:20:05 +0530

    Terraform-Roboshop

---
+++ 08-ACM/Variables.tf
@@ -0,0 +1,21 @@
+variable "instance_size" {
+    default = "t3.micro"
+}
+
+variable "project" {
+  default = "Roboshop"
+}
+
+variable "environment"{
+    default = "Dev"
+}
+
+
+variable "hosted_zone_id" {
+  default = "Z02025832NKTXYS2ALMGX"
+}
+
+variable "zone_name" {
+    default = "devopsaws.store"
+  
+}
